0a7eb8ec548698fee6b261b37595b20f61376675
herinta <herintya@gmail.com>
2024-12-04 16:48:10 +0700

    sanity setup

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -15,24 +15,7 @@ export default async function Home({
 
   const posts = await client.fetch(STARTUP_QUERY);
 
-  console.log(JSON.stringify(posts, null, 2));
-
-  // const posts = [
-  //   {
-  //     _createdAt: new Date(),
-  //     views: 55,
-  //     author: {
-  //       _id: 1,
-  //       name: "Rinta",
-  //     },
-  //     _id: 1,
-  //     description: "This is a description",
-  //     image:
-  //       "https://images.unsplash.com/photo-1593508512255-86ab42a8e620?q=80&w=1778&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //     category: "Robots",
-  //     title: "This is a title",
-  //   },
-  // ];
+  
 
   return (
     <>
